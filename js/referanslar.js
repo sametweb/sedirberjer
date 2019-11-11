1a7b517a6ff6963769e8c08f3ab2683d25e11e80
sametweb <sametmutevellioglu@gmail.com>
2019-11-11 00:11:57 -0800

    Referans resimler eklendi, JS olusturuldu

---
+++ js/referanslar.js
@@ -0,0 +1,18 @@
+//prettier-ignore
+const images = [
+  { title: '', description: '', path: `../img/referanslar/avusturya-restoran-2.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/avusturya-restoran.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/black-c.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/burgundy-sedir.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/cafe-chester-koltuk.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/cafe-chester.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/cafe-sandalye-sedir.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/demir-sedir-masa.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/kofteci-ramiz.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/lounge-sedir.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/okey-masasi.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/restoran-sandalye-masa.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/sedir-sandalye-masa.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/tahta-masa.jpeg`, otherImages: [] },
+  { title: '', description: '', path: `../img/referanslar/yesil-sedir.jpeg`, otherImages: [] }
+];
