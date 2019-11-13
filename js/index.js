8f5c4dae03d5e4291d46da3a3d05f14ff6371b85
sametweb <sametmutevellioglu@gmail.com>
2019-11-13 00:35:35 -0800

    Anasayfada ve referanslarda fullscreen eklendi

--- js/index.js
+++ js/index.js
@@ -1,20 +1,47 @@
+//prettier-ignore
+const images = [
+  {title: 'Cafe önü ahşap masa sandalye', imgURL: 'img/refs/thumb/ref1.jpg', originalImgURL: 'img/refs/original/ref1.png'},
+  {title: 'İç mekan cafe berjer sandalye masa', imgURL: 'img/refs/thumb/ref2.jpg', originalImgURL: 'img/refs/original/ref2.png'},
+  {title: 'Geniş mekan ikili sedir masa berjer', imgURL: 'img/refs/thumb/ref3.jpg', originalImgURL: 'img/refs/original/ref3.png'},
+  {title: 'Dış mekan cafe önü ahşap masa sandalye', imgURL: 'img/refs/thumb/ref4.jpg', originalImgURL: 'img/refs/original/ref4.png'},
+  {title: 'Açık hava demir cafe sedir koltuk masa', imgURL: 'img/refs/thumb/ref5.jpg', originalImgURL: 'img/refs/original/ref5.png'},
+  {title: 'Restoran ikili cafe koltuk masa', imgURL: 'img/refs/thumb/ref6.jpg', originalImgURL: 'img/refs/original/ref6.png'},
+  {title: 'Cafe koltuk sedir cam masa', imgURL: 'img/refs/thumb/ref7.jpg', originalImgURL: 'img/refs/original/ref7.png'},
+  {title: 'Cafe masa modern sandalye deri sedir', imgURL: 'img/refs/thumb/ref8.jpg', originalImgURL: 'img/refs/original/ref8.png'},
+  {title: 'Okey masası sandalye ahşap berjer', imgURL: 'img/refs/thumb/ref9.jpg', originalImgURL: 'img/refs/original/ref9.png'},
+  {title: 'Kapitoneli cafe sedir ahşap masa sandalye', imgURL: 'img/refs/thumb/ref10.jpg', originalImgURL: 'img/refs/original/ref10.png'},
+  {title: 'İki yanlı okey masası kumaş sandalye berjer', imgURL: 'img/refs/thumb/ref11.jpg', originalImgURL: 'img/refs/original/ref11.png'},
+  {title: 'Chester cafe ikili koltuk berjer lükens', imgURL: 'img/refs/thumb/ref12.jpg', originalImgURL: 'img/refs/original/ref12.png'}
+];
+
 const references = document.querySelector(".references");
 
-const createRef = imgURL => {
+const createRef = refObj => {
   let item = document.createElement("div");
   let img = document.createElement("img");
+  let overlay = document.createElement("div");
+  let originalImg = document.createElement("img");
+  let quitFullScreen = document.createElement("span");
 
   item.classList.add("item");
-  img.src = imgURL;
-  img.alt = "Hey";
+  img.src = refObj.imgURL;
+  originalImg.src = refObj.originalImgURL;
+  img.alt = refObj.title;
+  overlay.classList.add("overlay");
+  quitFullScreen.textContent = "X";
 
+  img.addEventListener("click", function() {
+    overlay.appendChild(originalImg);
+    document.body.prepend(overlay);
+  });
+  overlay.addEventListener("click", function(event) {
+    if (event.target == originalImg) return;
+    overlay.remove();
+  });
   item.appendChild(img);
+  overlay.appendChild(quitFullScreen);
 
   return item;
 };
 
-const totalRefs = 12;
-
-for (var i = 1; i <= totalRefs; i++) {
-  references.appendChild(createRef(`img/refs/thumb/ref${i}.jpg`));
-}
+images.forEach(item => references.appendChild(createRef(item)));
--- js/referanslar.js
+++ js/referanslar.js
@@ -1,35 +1,35 @@
 //prettier-ignore
 const images = [
-  { title: 'Avusturya', description: 'Bu kafe Avusturyada', location: 'Avusturya', path: `img/referanslar/avusturya-restoran-2.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/avusturya-restoran.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/black-c.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/burgundy-sedir.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/cafe-chester-koltuk.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/cafe-chester.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/cafe-sandalye-sedir.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/demir-sedir-masa.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/kofteci-ramiz.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/lounge-sedir.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/okey-masasi.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/restoran-sandalye-masa.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/sedir-sandalye-masa.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/tahta-masa.jpeg`, otherImages: [] },
-  { title: '', description: '', location: '', path: `img/referanslar/yesil-sedir.jpeg`, otherImages: [] }
-];
+    { title: 'Avusturya', description: 'Bu kafe Avusturyada', location: 'Avusturya', path: `img/referanslar/avusturya-restoran-2.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/avusturya-restoran.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/black-c.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/burgundy-sedir.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/cafe-chester-koltuk.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/cafe-chester.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/cafe-sandalye-sedir.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/demir-sedir-masa.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/kofteci-ramiz.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/lounge-sedir.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/okey-masasi.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/restoran-sandalye-masa.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/sedir-sandalye-masa.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/tahta-masa.jpeg`, otherImages: [] },
+    { title: '', description: '', location: '', path: `img/referanslar/yesil-sedir.jpeg`, otherImages: [] }
+  ];
 
 /*
-        <div class="ref">
-          <img src="img/refs/original/JPEG/ref1.jpg" alt="Referans 1" />
-          <div class="info">
-            <h3>Bilmem ne kafe</h3>
-            <p class="location">Berlin, Germany</p>
-            <p class="desc">
-              Bilmem ne kafe Berlin'in islek bir caddesinde populer nargile
-              kafelerinden bir tanesi.
-            </p>
+          <div class="ref">
+            <img src="img/refs/original/JPEG/ref1.jpg" alt="Referans 1" />
+            <div class="info">
+              <h3>Bilmem ne kafe</h3>
+              <p class="location">Berlin, Germany</p>
+              <p class="desc">
+                Bilmem ne kafe Berlin'in islek bir caddesinde populer nargile
+                kafelerinden bir tanesi.
+              </p>
+            </div>
           </div>
-        </div>
-*/
+  */
 
 let references = document.querySelector(".references");
 
@@ -40,16 +40,22 @@ const createRef = refObj => {
   let title = document.createElement("h3");
   let location = document.createElement("p");
   let desc = document.createElement("p");
+  let overlay = document.createElement("div");
+  let quitFullScreen = document.createElement("span");
+  let fullscreenImg = document.createElement("img");
 
   ref.classList.add("ref");
   info.classList.add("info");
   location.classList.add("location");
   desc.classList.add("p");
+  overlay.classList.add("overlay");
 
   img.src = refObj.path;
+  fullscreenImg.src = refObj.path;
   title.textContent = refObj.title;
   location.textContent = refObj.location;
   desc.textContent = refObj.description;
+  quitFullScreen.textContent = "X";
 
   info.appendChild(title);
   info.appendChild(location);
@@ -57,7 +63,27 @@ const createRef = refObj => {
   ref.appendChild(img);
   ref.appendChild(info);
 
+  ref.addEventListener("click", function() {
+    overlay.appendChild(quitFullScreen);
+    overlay.appendChild(fullscreenImg);
+    document.body.prepend(overlay);
+  });
+  overlay.addEventListener("click", function(event) {
+    if (event.target == fullscreenImg) return;
+    overlay.remove();
+  });
   return ref;
 };
 
 images.forEach(item => references.appendChild(createRef(item)));
+
+let allImages = document.querySelectorAll(".references img");
+
+setTimeout(
+  () =>
+    allImages.forEach(
+      img =>
+        (img.style.transform = `translateY(-${(img.clientHeight - 350) / 2}px)`)
+    ),
+  1000
+);
